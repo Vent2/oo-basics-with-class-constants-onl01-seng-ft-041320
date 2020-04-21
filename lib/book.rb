8b9c6ee47cf11f241b25147ca3429ba3924fee89
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-21 19:03:23 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,6 +2,8 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
@@ -10,4 +12,17 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+  def genre=(book_genre)
+    @genre = book_genre
+    GENRES << book_genre
+  end
+
 end
+
+class Dog
+  def initialize(bark)
+    puts "woof"
+  end
+end
+
+fido = Dog.new
