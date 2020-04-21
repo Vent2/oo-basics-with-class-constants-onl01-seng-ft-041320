6a970ceb9db1e6fdbf16b9786f3edad79345adf7
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-21 19:06:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRAND = []
   def initialize(brand)
     @brand = brand
   end
@@ -11,4 +11,7 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  def uni_brand=(uni_brand)
+    BRAND << 
+  
 end
