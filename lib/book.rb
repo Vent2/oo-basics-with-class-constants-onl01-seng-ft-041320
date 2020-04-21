af59a9eeed7499899e88df65d6ed5424d547da10
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-21 19:13:56 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,12 +2,19 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
   end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+
+  def genre=(book_genre)
+    @genre = book_genre
+    GENRES << book_genre
   end
 
 end
