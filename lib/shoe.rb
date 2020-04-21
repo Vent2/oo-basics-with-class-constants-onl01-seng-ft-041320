53d5f64eef74924bef7867418e9e21c526e896fe
Gerald Ventura <ventura.c.gerald@gmail.com>
2020-04-21 19:12:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,18 +1,18 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  BRAND = []
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
+    if !(BRANDS.include?(brand)) 
+       BRANDS << brand 
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-  def uni_brand=(uni_brand)
-    @brand = uni_brand
-    BRAND <<
+    
 
 end
